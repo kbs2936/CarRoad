e6a426880da4fe0ef1c6d3994a10a232d277feb0
刘煌 <225225@nd.com>
2022-06-23 09:10:28 +0800

    板载LED调试成功

--- CarRoad.ino
+++ CarRoad.ino
@@ -11,11 +11,12 @@ int btnCount = 0;
  */
 void setup()
 {
-    // LED和按键引脚定义，默认上拉，低电平有效
-    // pinMode(LED_PIN, OUTPUT);
-    // pinMode(BTN_PIN, INPUT);
+    // LED和按键引脚定义，默认上拉，低电平有效。板载LED默认下拉，也是低电平有效
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BTN_PIN, INPUT);
+    attachInterrupt(BTN_PIN, btnHandler, FALLING);
     pinMode(BOARD_LED_PIN, OUTPUT);
-    // attachInterrupt(BTN_PIN, btnHandler, LOW);
+    ledOff();
 }
 
 /**
@@ -23,20 +24,15 @@ void setup()
  */
 void loop()
 {
-    // if (btnCount > 0)
-    // {
-    //     // btnCount = 2;
-    //     // delay(1000);
-    //     digitalWrite(LED_PIN, LOW);
-    //     digitalWrite(BOARD_LED_PIN, LOW);
-    //     delay(1000);
-    //     return;
-    // }
-
-    digitalWrite(BOARD_LED_PIN, HIGH);
-    delay(1000);
-    digitalWrite(BOARD_LED_PIN, LOW);
-    delay(1000);
+    if (btnCount % 2)
+    {
+        ledOn();
+    }
+    else
+    {
+        ledOff();
+    }
+    delay(500);
 }
 
 /**
@@ -45,4 +41,16 @@ void loop()
 void btnHandler()
 {
     btnCount++;
+}
+
+void ledOn()
+{
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(BOARD_LED_PIN, LOW);
+}
+
+void ledOff()
+{
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(BOARD_LED_PIN, HIGH);
 }
